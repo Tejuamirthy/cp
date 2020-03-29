940c127bd3b6cbb1d96af1d89f8d659f0a83088e
Teju Amirthy <tejuamirthi@gmail.com>
2020-03-29 23:01:32 +0530

    Optimized approach for duplicates in an array
     Works only when all the elements are in range[0,n)

--- arrays/PrintDuplicates.java
+++ arrays/PrintDuplicates.java
@@ -3,9 +3,11 @@ public class PrintDuplicates {
         int[] arrOne = { 0, 3, 1, 2 };
         int[] arrTwo = { 2, 3, 1, 2, 3 };
         int[] arrThree = { 2, 3, 1, 2, 3, 2 };
-        printDuplicates(arrOne);
-        printDuplicates(arrTwo);
-        printDuplicates(arrThree);
+//         printDuplicates(arrOne);
+//         printDuplicates(arrTwo);
+//         printDuplicates(arrThree);
+        printDuplictesOptimized(arrThree);
+        printDuplictesOptimized(arrOne);
     }
 
     static void printDuplicates(int[] arr) {
@@ -25,6 +27,26 @@ public class PrintDuplicates {
             }
             arr[i] = -1;
         }
+        System.out.println(exists ? "" : "-1");
+    }
+    
+    static void printDuplictesOptimized(int[] arr) {
+        // works only when all the elements in range[0,n)
+        int n = arr.length;
+        boolean exists = false;
+        for(int i = 0; i < n; i++) {
+            int x = (arr[i]+n)%n;
+            
+            arr[x] += n;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(arr[i]/n >= 2) {
+                exists = true;
+                System.out.print(i+" ");
+            }
+        }
+        
         System.out.println(exists ? "" : "-1");
     }
 }
